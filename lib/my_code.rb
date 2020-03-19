57d9aeabef6608ec165f1e062a7032fa8f6be78f
Robert Phillips <rpdecks@gmail.com>
2020-03-19 19:59:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,6 @@ def reduce(array, starting_point = nil)
     total = array[0]
     i = 1 
   end
-  total = starting_point
   i = 0 
   while i < array.length do 
     total = yield(total, array[i])
