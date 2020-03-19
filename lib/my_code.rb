0aee42853e9297d9af6b37ce9593e1a2c161565d
Robert Phillips <rpdecks@gmail.com>
2020-03-19 20:00:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+require 'pry'
+
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length do 
+    new_array.push(yield(array[i]))
+    i += 1 
+  end
+  new_array
+end
+
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    total = starting_point
+    i = 0 
+  else
+    total = array[0]
+    i = 1 
+  end
+  while i < array.length do 
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end
+
